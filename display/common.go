f693bb1379e208839adcef613623e620f723f65f
Yegor Stepanov <ystepanoff@icloud.com>
2025-05-11 01:36:29 +0100

    Fix linting errors

---
+++ display/common.go
@@ -0,0 +1,17 @@
+package display
+
+func Init() error {
+	return nil
+}
+
+func Clear() error {
+	return nil
+}
+
+func Display() error {
+	return nil
+}
+
+func Update(batteryLevel uint8) error {
+	return nil
+}
